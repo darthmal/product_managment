24a6ca29dc491d6b0368d0e45597c806fd8e2e86
Salam <salamabou88@gmail.com>
2025-03-13 13:32:22 +0100

    feat: fixing mapper, add liquibase changelog

--- src/main/java/com/mapper/ProductMapper.java
+++ src/main/java/com/mapper/ProductMapper.java
@@ -1,17 +1,29 @@
 package com.mapper;
 
-import com.dto.ProductDTO;
+import com.dto.ProductRequestDto;
+import com.dto.ProductResponseDto;
 import com.model.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
-public interface ProductMapper {
-    ProductDTO toDto(Product product);
-    
-    Product toEntity(ProductDTO productDTO);
-    
+public abstract class ProductMapper {
+    public abstract ProductResponseDto toDto(Product product);
+
+    public abstract List<ProductResponseDto> toDtoList(List<Product> entities);
+
+    @Mapping(target = "createdAt", expression = "java(com.domain.project.common.utils.DateTimeUtils.now())")
+    @Mapping(target = "updatedAt", expression = "java(com.domain.project.common.utils.DateTimeUtils.now())")
+    public abstract Product toEntity(ProductRequestDto productDTO);
+
     @Mapping(target = "id", ignore = true)
-    void updateEntityFromDto(ProductDTO productDTO, @MappingTarget Product product);
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", expression = "java(com.domain.project.common.utils.DateTimeUtils.now())")
+    @Mapping(target = "name", expression = ("java(source.getName() != null && !source.getName().isEmpty()  ? source.getName() : target.getName())"))
+    @Mapping(target = "price", expression = ("java(source.getPrice() != null && !source.getPrice().equals(0)  ? source.getPrice() : target.getPrice())"))
+    @Mapping(target = "stockQuantity", expression = ("java(source.getStockQuantity() != null && !source.getStockQuantity().equals(0)  ? source.getStockQuantity() : target.getStockQuantity())"))
+    public abstract Product update(Product source, @MappingTarget Product target);
 }
